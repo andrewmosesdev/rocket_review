dc37bd752e8c8eec90dfd166c3677283b76ef711
Andrew Moses <andrewmosesdrive@gmail.com>
2021-02-21 17:21:38 -0700

    removed spark component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import { SprkButton } from '@sparkdesignsystem/spark-react';
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
         >
           Learn React
         </a>
-        <SprkButton loading>Submit</SprkButton>
 
       </header>
     </div>
