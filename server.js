b2cf944c3b8f5d7c256b5ca42ab7fb16d83b37e2
Andrew Moses <andrewmosesdrive@gmail.com>
2021-02-27 15:56:55 -0700

    restructured server.js

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 require('./utils/verifyConfiguration')();
 const express = require('express');
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
 const path = require('path');
+const routes = require('./controllers')
 const PORT = process.env.PORT || 3001;
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const compression = require('compression');
 const app = express();
+
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'));
@@ -17,7 +18,6 @@ if (process.env.NODE_ENV !== 'production') {
 app.use('/admin', require('./utils/admin'));
 
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,25 +25,19 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
+app.use(routes);
 
-mongoose
-    .connect(process.env.MONGODB_URI || 'mongodb://localhost/rocket_review', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    })
-    .then(() => {
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
-        const routes = require('./controllers');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rocket_review', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
 
-        app.use(routes);
-
-        app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, './client/build/index.html'));
-        });
-
-        app.listen(PORT, function () {
-            console.log(`Server now on port ${PORT}!`);
-        });
-    });
+app.listen(PORT, function () {
+    console.log(`Server now on port ${PORT}!`);
+});
