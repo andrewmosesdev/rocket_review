367df7fe9df2c74726a7f39a67cb1631a3e6cb85
Andrew Moses <andrewmosesdrive@gmail.com>
2021-03-28 18:41:56 -0700

    chart displays styled slightly

---
+++ client/src/components/SubTopicChart.js
@@ -0,0 +1,165 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Pie } from 'react-chartjs-2';
+import axios from 'axios';
+
+const SubTopicChart = (props) => {
+    const [subTopicData, setSubTopicData] = useState([]);
+    const [refresh, toggleRefresh] = useState(0);
+    console.log(subTopicData)
+
+    let talliedSubTopic = {
+        history: 0,
+        whosWho: 0,
+        mloTimeline: 0,
+        agencies: 0,
+        yourLicense: 0,
+        respa: 0,
+        products: 0,
+        mortgageMath1: 0,
+        programs: 0,
+        ecoa: 0,
+        consumerContactLaws: 0,
+        borrowerEthicsAndFraud: 0,
+        industryFraud: 0,
+        industryEthics: 0,
+        insurances: 0,
+        thirdPartyServices: 0,
+        mortgageMath2: 0,
+        tila: 0,
+        fairnessLaws: 0,
+        financialCrimesLaws: 0,
+        disclosuresAndDocuments: 0,
+        closing: 0,
+        remittanceAndOwnership: 0,
+        repayment: 0,
+        trid: 0,
+        vocab: 0,
+        temporaryAuthority: 0
+    };
+
+    for(let i = 0; i < subTopicData.length; i++){
+        if(subTopicData[i].subTopic == 'History'){
+            talliedSubTopic.history ++;
+        } 
+        else if(subTopicData[i].subTopic == "Who's Who?"){
+            talliedSubTopic.whosWho ++;
+        } 
+        else if(subTopicData[i].subTopic == 'mloTimeline'){
+            talliedSubTopic.mloTimeline ++;
+        }
+        else if(subTopicData[i].subTopic == 'Agencies'){
+            talliedSubTopic.agencies ++;
+        }
+        else if(subTopicData[i].subTopic == 'Your License'){
+            talliedSubTopic.yourLicense ++;
+        }
+        else if(subTopicData[i].subTopic == 'RESPA'){
+            talliedSubTopic.respa ++;
+        }
+        else if(subTopicData[i].subTopic == 'Products'){
+            talliedSubTopic.products ++;
+        }
+        else if(subTopicData[i].subTopic == 'Mortgage Math 1'){
+            talliedSubTopic.mortgageMath1 ++;
+        }
+        else if(subTopicData[i].subTopic == 'Programs'){
+            talliedSubTopic.programs ++;
+        }
+        else if(subTopicData[i].subTopic == 'ECOA'){
+            talliedSubTopic.ecoa ++;
+        }
+        else if(subTopicData[i].subTopic == 'Consumer Contact Laws'){
+            talliedSubTopic.consumerContactLaws ++;
+        }
+        else if(subTopicData[i].subTopic == 'Borrower Ethics and Fraud'){
+            talliedSubTopic.borrowerEthicsAndFraud ++;
+        }
+        else if(subTopicData[i].subTopic == 'Industry Fraud'){
+            talliedSubTopic.industryFraud ++;
+        }
+        else if(subTopicData[i].subTopic == 'Industry Ethics'){
+            talliedSubTopic.industryEthics ++;
+        }
+        else if(subTopicData[i].subTopic == 'Insurances'){
+            talliedSubTopic.insurances ++;
+        }
+        else if(subTopicData[i].subTopic == 'Third Party Services'){
+            talliedSubTopic.thirdPartyServices ++;
+        }
+        else if(subTopicData[i].subTopic == 'Mortgage Math 2'){
+            talliedSubTopic.mortgageMath2 ++;
+        }
+        else if(subTopicData[i].subTopic == 'TILA'){
+            talliedSubTopic.tila ++;
+        }
+        else if(subTopicData[i].subTopic == 'Fairness Laws'){
+            talliedSubTopic.fairnessLaws ++;
+        }
+        else if(subTopicData[i].subTopic == 'Financial Crimes Laws'){
+            talliedSubTopic.financialCrimesLaws ++;
+        }
+        else if(subTopicData[i].subTopic == 'Disclosures and Documents'){
+            talliedSubTopic.disclosuresAndDocuments ++;
+        }
+        else if(subTopicData[i].subTopic == 'Closing'){
+            talliedSubTopic.closing ++;
+        }
+        else if(subTopicData[i].subTopic == 'Remittance and Ownership'){
+            talliedSubTopic.remittanceAndOwnership ++;
+        }
+        else if(subTopicData[i].subTopic == 'Repayment'){
+            talliedSubTopic.repayment ++;
+        }
+        else if(subTopicData[i].subTopic == 'TRID'){
+            talliedSubTopic.trid ++;
+        }
+        else if(subTopicData[i].subTopic == 'Vocab'){
+            talliedSubTopic.vocab ++;
+        }
+        else {
+            talliedSubTopic.temporaryAuthority ++;
+        }
+    }
+    console.log(talliedSubTopic)
+
+    let talliedSubTopicArray = Object.values(talliedSubTopic);
+
+    // console.log(talliedSubTopicArray)
+
+    useEffect(() => {
+        fetchReviewItems();
+    }, [refresh]);
+    
+    async function fetchReviewItems() {
+        const { data } = await axios.get('/api/revObjs');
+        setSubTopicData(data);
+        // console.log(subTopicData)
+    };
+
+    let chartData = {
+        labels: ['History', "Who's Who?", 'MLO Timeline', 'Agencies', 'Your License', 'RESPA', 'Products', 'Mortgage Math 1', 'Programs', 'ECOA', 'Consumer Contact Laws', 'Borrower Ethics and Fraud', 'Industry Fraud', 'Insurances', 'Third Party Services', 'Mortgage Math 2', 'TILA', 'Fairness Laws', 'Financial Crimes Laws', 'Disclosures and Documents', 'Closing', 'Remittance and Ownership', 'Repayment', 'TRID', 'Vocab', 'Temporary Authority'],
+        datasets: [{
+            data: talliedSubTopicArray,
+            backgroundColor: ['#D6D2D2', '#F1E4F3', '#F4BBD3', '#1A1423', '#3D314A', '#684756', '#96705B', '#001514', '#6B0504', '#A3320B', '#E6AF2E', '#C4B2BC', '#A29587', '#846C5B', '#332E3C', '#94B9AF', '#942911', '#FE4E00', '#89043D', '#2FE6DE', '#1C3041', '#78C0E0', '#150578', '#3943B7', '#88527F', '#614344']
+        }]
+    }
+
+    return (
+
+        <div>
+            <Pie
+                data={{
+                    labels: chartData.labels,
+                    datasets: chartData.datasets,
+                    
+                }}
+            />
+        </div>
+    )
+
+
+}
+
+
+export default SubTopicChart;
--- client/src/pages/CreateReviewObjects.js
+++ client/src/pages/CreateReviewObjects.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react';
 import API from '../utils/API';
 import SubmitForm from '../components/SubmitForm';
 import DifficultyChart from '../components/DifficultyChart';
-import TopicChart from '../components/TopicChart'
+import TopicChart from '../components/TopicChart';
+import SubTopicChart from '../components/SubTopicChart';
+import { SprkStack, SprkStackItem } from "@sparkdesignsystem/spark-react";
 
 const CreateReviewObjects = function () {
     const [revObjs, setRevObjs] = useState([]);
@@ -22,16 +24,46 @@ const CreateReviewObjects = function () {
     console.log('need to add charts on question submission page')
 
     return (
-        <div style={{display: 'flex'}}>
-            <div>
-                <SubmitForm />
-            </div>
-            <div style={{}}>
-                <DifficultyChart />
-            </div>
-            <div style={{}}>
-                <TopicChart />
-            </div>
+        <div style={{}}>
+            <SprkStack 
+            splitAt="tiny"
+            
+            >
+                <SprkStackItem additionalClasses="sprk-o-Stack__item--half@xs">
+                    <div style={{ width: '100%' }}>
+                        <SubmitForm />
+                    </div>
+                </SprkStackItem>
+                <SprkStackItem additionalClasses="sprk-o-Stack__item--half@xs">
+
+                    <div>
+                        <SprkStack
+                        itemSpacing='large'
+                        additionalClasses='sprk-o-Stack--center-row'
+                        >
+                            <SprkStackItem>
+                                <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+                                    
+                                    <DifficultyChart />
+                                    
+                                    <TopicChart />
+                                </div>
+
+
+                            </SprkStackItem>
+                            <br/>
+                            <br/>
+                            <SprkStackItem>
+                                
+                                <SubTopicChart />
+
+                            </SprkStackItem>
+                        </SprkStack>
+
+                    </div>
+
+                </SprkStackItem>
+            </SprkStack>
         </div>
     );
 };
