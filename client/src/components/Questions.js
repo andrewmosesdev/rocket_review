72c12587d376b8a338cfed5b913f7aa4f0b6d927
Andrew Moses <andrewmosesdrive@gmail.com>
2021-03-30 21:39:53 -0700

    updated result card to have divider and improved font weight

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SprkPromo, SprkButton } from "@sparkdesignsystem/spark-react";
+import { SprkPromo, SprkButton, SprkDivider } from "@sparkdesignsystem/spark-react";
 import API from '../utils/API';
 
 const Questions = ({ result }) => {
@@ -14,27 +14,29 @@ const Questions = ({ result }) => {
 
     return (
 
-        <div key={result._id} style={{borderStyle: 'solid', borderRadius: '5px'}}>
+        <div key={result._id} style={{ borderStyle: 'solid', borderRadius: '5px' }}>
 
             <SprkPromo
                 title={result.question}
-                subtitle={ <div>  {result.difficulty}, {result.topic}, {result.subTopic} </div>}
+                subtitle={<div>  {result.difficulty}, {result.topic}, {result.subTopic} </div>}
                 additionalClasses='sprk-o-Stack--split@s'
                 hasBorder
                 idString={result._id}
             >
-                {result.answer}
+                <SprkDivider idString="divider-1" element="span" />
+                <div style={{fontWeight: 'bold'}}>{result.answer}</div>
                 <br />
                 <br />
-                {result.isFlagged ? <h7> <div style={{borderStyle: 'solid', paddingTop: '3px', paddingBottom: '3px', paddingRight: '5px', paddingLeft: '5px', display: 'inline-block', backgroundColor: '#C70039', color: 'white', borderRadius: '5px'}}> <strong> Warning: This has been flagged as incorrect or incomplete! </strong> </div></h7> 
-                : 
-                <SprkButton
-                    idString="button-1"
-                    analyticsString="button-1-analytics"
-                    id={result._id}
-                    onClick={toggleFlagged}
-                >
-                    Flag
+                
+                {result.isFlagged ? <h7> <div style={{ borderStyle: 'solid', paddingTop: '3px', paddingBottom: '3px', paddingRight: '5px', paddingLeft: '5px', display: 'inline-block', backgroundColor: 'rgb(200, 16, 46)', color: 'white', borderRadius: '5px' }}> <strong> Warning: This has been flagged as incorrect or incomplete! </strong> </div></h7>
+                    :
+                    <SprkButton
+                        idString="button-1"
+                        analyticsString="button-1-analytics"
+                        id={result._id}
+                        onClick={toggleFlagged}
+                    >
+                        Flag
                 </SprkButton>}
 
             </SprkPromo>
